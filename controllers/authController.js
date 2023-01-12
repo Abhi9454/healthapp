ecf5867d9d47afc5b89b25906f1cfd337b20a2b9
arjun <arjun.nair@accubits.com>
2023-01-12 16:47:55 +0530

    added sigup and login aapi

---
+++ controllers/authController.js
@@ -0,0 +1,91 @@
+const userModel = require('../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+    signUp : async function(req, res){
+        try {
+            const {name,phone,email,password,userType} = req.body;
+           let user = await userModel.findOne({email});
+           if (user) return res.status(400).json({ success : false, message: "Email Already Exists"});
+           user =  new userModel({name,phone,email,password,userType,typeVendor,category})
+           const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            const payload = {id: user.id};
+            jwt.sign(
+                    payload,
+                    "healthapp", {
+                        expiresIn: '10d'
+                    },
+                    (err, token) => {
+                        if (err) throw err;
+                    res.status(200).json({
+                    success: true, message : 
+                    [{
+                        token : token,
+                        name  : user.name,
+                        email : user.email,
+                        phoneNumber : user.phone,
+                        userType : user.userType,
+                    }]
+                    });
+                    }
+            );
+           }
+           catch (error) {
+               res.status(400).json({success : false,message: error.message})
+           }
+    },
+
+    login : async function(req, res){
+        try {
+            const {email,password} = req.body;
+           let user = await userModel.findOne({email, active : 1});
+           if (user) {
+                const validPassword = await bcrypt.compare(password, user.password);
+               if(validPassword){
+                            const payload = {
+                                   id: user.id
+                           };
+       
+                        jwt.sign(
+                               payload,
+                               "healthapp", {
+                                   expiresIn: '10d'
+                               },
+                           (err, token) => {
+                                   if (err) throw err;
+                                   //sendEmail("mishra.abhi8888@gmail.com")
+                                   res.status(200).json({
+                                           success: true, message :
+                                               [{	
+                                                  token : token,
+                                                 name  : user.name,
+                                                  email : user.email,
+                                                  phoneNumber : user.phone,
+                                                  userType : user.userType,
+                                               }]
+                                       });
+                               }
+                       );
+               }
+               else{
+                   return res.status(200).json({
+                                       success : false, message: "Invalid email/password"
+                               });
+               }
+                }
+            else
+                {
+                return res.status(400).json({
+                   success:false, message:"No user exists"
+                    });
+                }
+           }
+           catch (error) {
+               res.status(400).json({success : false,message: error.message})
+           }
+    },
+}
+
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 
 router.route('/getallusers').get(userController.getallUsers);
 router.route('/getuserdetail').post(userController.getUserDetails);
+
+
+router.route('/signUp').get(authController.signUp);
+router.route('/login').post(authController.login);
